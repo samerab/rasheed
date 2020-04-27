340a9563e368402c54a1a79fc59ef963fbe54d4f
ablasamer <dersyrische@gmail.com>
2020-04-27 22:40:52 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'first';
+export class AppComponent implements OnInit {
+  
+  names = [10, 7, 12];
+  names2 = ['sam', 'ff', 'zzz'];
+  contacts= [
+    { 
+      name: 'Rasheed', 
+      phone: '23525', 
+      photo: 'https://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349162/8d661276bec81ad58ad5140bf91aad04.jpg'
+    },
+    { 
+      name: 'Samer', 
+      phone: '55555', 
+      photo: 'https://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349162/8d661276bec81ad58ad5140bf91aad04.jpg'
+    },
+    { 
+      name: 'Jasem', 
+      phone: '888888', 
+      photo: 'https://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349162/8d661276bec81ad58ad5140bf91aad04.jpg'
+    },
+    { 
+      name: 'Jasem', 
+      phone: '888888', 
+      photo: 'https://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349162/8d661276bec81ad58ad5140bf91aad04.jpg'
+    },
+    { 
+      name: 'Jasem', 
+      phone: '888888', 
+      photo: 'https://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349162/8d661276bec81ad58ad5140bf91aad04.jpg'
+    },
+    { 
+      name: 'Jasem', 
+      phone: '888888', 
+      photo: 'https://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349162/8d661276bec81ad58ad5140bf91aad04.jpg'
+    },
+    { 
+      name: 'Jasem', 
+      phone: '888888', 
+      photo: 'https://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349162/8d661276bec81ad58ad5140bf91aad04.jpg'
+    }
+  ];
+  
+
+  ngOnInit() {
+  }
+
+  change() {
+  }
+
+  
+
+
+    
+
+
 }
